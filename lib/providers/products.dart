c4a45a40cb667b63579d9d8ce66eb3c273672f45
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-04-03 10:22:33 +0530

    changed the url

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -68,7 +68,8 @@ class Products with ChangeNotifier {
   // }
 
   Future<void> addProduct(Product product) async {
-    const url = 'https://flutter-update.firebaseio.com/products';
+    const url =
+        'https://shopflutter-43fb5-default-rtdb.firebaseio.com/products.json';
     try {
       final response = await http.post(
         url,
