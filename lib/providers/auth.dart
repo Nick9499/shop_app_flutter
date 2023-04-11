690a2a25959a7bd8aca6810b16af17999f0afd3a
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-04-11 09:02:33 +0530

    handling authentication error

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_complete_guide/models/http_exception.dart';
 import 'package:http/http.dart' as http;
 
 const API_KEY = 'AIzaSyCAabg-rfEo5Kj3VnkSCQl9yNdZwKCvtBE';
@@ -11,29 +12,34 @@ class Auth with ChangeNotifier {
   String _userId;
 
   Future<void> _authenticate(
-    String email,
-    String password,
-    String urlSegment,
-  ) async {
+      String email, String password, String urlSegment) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$API_KEY';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=$API_KEY';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
-    return _authenticate(email, password, 'signUp');
+    return _authenticate(email, password, 'signupNewUser');
   }
 
   Future<void> login(String email, String password) async {
-    return _authenticate(email, password, 'signInWithPassword');
+    return _authenticate(email, password, 'verifyPassword');
   }
 }
