3516b5196bc0dfaf2af487a2484590a895ae737b
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-04-11 07:52:44 +0530

    added login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,14 +3,20 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+const API_KEY = 'AIzaSyCAabg-rfEo5Kj3VnkSCQl9yNdZwKCvtBE';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCAabg-rfEo5Kj3VnkSCQl9yNdZwKCvtBE';
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlSegment,
+  ) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$API_KEY';
     final response = await http.post(
       url,
       body: json.encode(
@@ -21,5 +27,13 @@ class Auth with ChangeNotifier {
         },
       ),
     );
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
