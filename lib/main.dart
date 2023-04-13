01d629b6c5f6bc85bc31d77ed7546c19259e5772
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-04-13 10:45:45 +0530

    Attaching orders to user & adding logout

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ class MyApp extends StatelessWidget {
           create: (context) => null,
           update: (ctx, auth, previousOrders) => Orders(
             auth.token,
+            auth.userId,
             previousOrders == null ? [] : previousOrders.orders,
           ),
         ),
